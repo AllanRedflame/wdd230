082a68ad5e697cf81e0e10d7b362656c7214289b
AllanRedflame <allanredflame@gmail.com>
2023-04-25 17:41:56 -0600

    Added location

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,6 +2,6 @@ const year = new Date().getFullYear();
 const modified = document.lastModified;
 const thingy = document.getElementById("last-updated");
 
-document.querySelector("p").innerHTML = "&copy " + year; 
+document.querySelector("p").innerHTML = "&copy " + year + " David Poulsen | Utah"; 
 document.getElementById("last-updated").innerHTML = "Last updated: " + modified;
 
